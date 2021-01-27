5f7c9b5c12e03598ddcf6d6dc5595ba776780be7
Remian Feral <53864440+Remian-Feral@users.noreply.github.com>
2021-01-27 15:57:24 +0800

    Create index.php

---
+++ Our Project/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+  <br/>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Login App</title>
+<link href="logn.css" rel="stylesheet" type="text/css" />
+<link href="https://fonts.googleapis.com/css?family=play" rel="stylesheet">
+</head>
+
+<body>
+	<link href="logn.css" rel="stylesheet"/>
+	<div class="signin" align="center" color="Black" >
+		<form>
+			<h2 style="color:"#825201">Log In</h2>
+			<input type="text" name="username" placeholder="Username">
+			<input type="password" name="pass" placeholder="Password"><br><br>
+			
+			<a href="cong.html"><input type="button" value="Log In"></a>
+			<br>
+			<br>
+			<div id="container">
+			<a href="forgot.html" style=" margin-right;0px font-size: 12px;
+			font-family:Tahoma, Geneva, sans-serif;">Forget password</a>
+			</div>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			
+			<font style="color:#808080"><b>Don't have account?</b></font><a href="register.php" ><b>&nbsp;Sign Up</b></a>
+		
+		</form>
+	</div>
+		<?php include('inc/footer.php'); ?>
