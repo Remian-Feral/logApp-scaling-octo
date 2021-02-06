0a8a891ce4a61e8471a3bbe42d6f4df5080f2511
Remian Feral <53864440+Remian-Feral@users.noreply.github.com>
2021-02-06 16:08:40 +0800

    Update footer.php

--- inc/footer.php
+++ inc/footer.php
@@ -1,7 +1,8 @@
 <footer class="mastfoot mt-auto" style="text-align: center;">
     <div class="inner">
-      <p>Created by <a href="https://github.com/kjvmartinez">kjmartinez</a> for BSCS 3B1</p>
+      <p>Created by <a href="https://github.com/Remian-Feral/logApp-scaling-octo.git">Ferrer,Caabay,Atanoso</a> for BSCS 3B1</p>
     </div>
 </footer>
   </body>
 </html>
+© 2021 GitHub, Inc.
