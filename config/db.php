b0c351814b0238268a3c0976117edf48e9e55ec7
Remian Feral <53864440+Remian-Feral@users.noreply.github.com>
2021-02-06 16:09:53 +0800

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,9 +1,10 @@
 <?php
 	// Create Connection
-	$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+	$conn = mysqli_connect('localhost', 'root', '', 'db');
 
 	// Check Connection
 	if(mysqli_connect_errno()){
 		// Connection Failed
 		echo 'Failed to connect to MySQL '. mysqli_connect_errno();
 	}
+?>
