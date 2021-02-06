3ab91cd2feaef0130d60bed29a0ef40538506bf0
Remian Feral <53864440+Remian-Feral@users.noreply.github.com>
2021-02-06 16:10:15 +0800

    Delete Our Project directory

--- Our Project/inc/navbar.php
+++
@@ -1,8 +0,0 @@
-
-<!-- Image and text -->
-<nav class="navbar navbar-light bg-light">
-  <a class="navbar-brand" href="index.php">
-    <img src="img/bootstrap.svg" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">
-    Log Application
-  </a>
-</nav>
